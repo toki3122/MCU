ba138a60f3bd1f0d045f5bcedc6383ebb7b74323
toki3122 <160049158+toki3122@users.noreply.github.com>
2026-03-29 00:02:54 +0600

    Refactor WiFi scanning and add loop function

--- esp32_RSSIcheck.ino
+++ esp32_RSSIcheck.ino
@@ -1,18 +1,15 @@
 #include <WiFi.h>
-
 void setup() {
   Serial.begin(115200);
   while (!Serial) delay(10);
-
   WiFi.mode(WIFI_STA);
   WiFi.setSleep(false);
-
   int n = WiFi.scanNetworks();
   Serial.println("Scan done");
-
   if (n == 0) {
     Serial.println("NO NETWORKS FOUND");
-  } else {
+  } 
+  else {
     for (int i = 0; i < n; i++) {
       Serial.print(WiFi.SSID(i));
       Serial.print("  RSSI=");
@@ -20,13 +17,4 @@ void setup() {
     }
   }
 }
-// Tokee  RSSI=-37
-// c6665dea  RSSI=-68
-// OPPO A76  RSSI=-76
-// Siddique Bari  RSSI=-80
-// Tuhin  RSSI=-87
-// Beauti  RSSI=-89
-// Ka If   RSSI=-91
-// Milon  RSSI=-92
-// Samir(Green Tech)  RSSI=-93
 void loop() {}
