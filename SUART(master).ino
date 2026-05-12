46a000ce7336aead79ddea1d3303fff49e614dbb
toki3122 <160049158+toki3122@users.noreply.github.com>
2026-05-12 07:39:55 +0600

    Add SUART Arduino sketch for voltage and current measurement
    Initialize SoftwareSerial and read analog values to calculate voltage and current.

---
+++ SUART(master).ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+SoftwareSerial SUART(5, 6); //SRX = 5, STX = 6
+float V,I;
+void setup(){
+SUART.begin(9600);
+float V1=(5.0/1023)*analogRead(A0);
+float V2=(5.0/1023)*analogRead(A1);
+V = V2-V1;
+I = V/2.2; //unit mA 
+}
+void loop(){
+SUART.print('<');
+SUART.print(V,2);
+SUART.print(',');
+SUART.print(I,2);
+SUART.print(‘>’);
+delay(1000);
+}
